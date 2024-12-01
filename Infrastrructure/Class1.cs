c9e8a05264bb3a4c3b79efbc7873ea8dcab6c489
HOME\sean2 <wkus963@gmail.com>
2024-12-01 14:58:42 +0800

    add separate project test example

--- Infrastrructure/Class1.cs
+++ Infrastrructure/Class1.cs
@@ -2,6 +2,14 @@
 {
     public class Class1
     {
+        public void Foo()
+        {
+            Console.WriteLine("foo");
+        }
 
+        public void Bar() 
+        { 
+            Console.WriteLine("bar");
+        }
     }
 }
---
+++ InfrastrructureTests1/Class1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Infrastrructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastrructure.Tests
+{
+    public class Class1Tests
+    {
+        [Fact()]
+        public void FooTest()
+        {
+            var foo = new Class1();
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
