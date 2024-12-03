1f86b89dcd103d9ca7a2ab551041a7c65a185638
HOME\sean2 <wkus963@gmail.com>
2024-12-03 22:39:15 +0800

    add IRepository

---
+++ Domain.Models/Db/TCoeusDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Db
+{
+    public class TCoeusDbContext : DbContext
+    {
+        public TCoeusDbContext(DbContextOptions<TCoeusDbContext> options) : base(options)
+        {
+        }
+
+        // 定義資料表對應
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace WebApi
 {
     public class Program
@@ -13,6 +15,9 @@ namespace WebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<MyDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
